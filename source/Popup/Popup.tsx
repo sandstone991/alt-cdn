425fff8ea746427a1800d8dedae372f8f0509d13
sandstone991 <ahmed195138@feng.bu.edu.eg>
2023-08-31 17:25:36 +0300

    done for now.

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -9,44 +9,32 @@ function openWebPage(url: string): Promise<Tabs.Tab> {
 
 const Popup: React.FC = () => {
     return (
-        <section id="popup">
-            <h2>WEB-EXTENSION-STARTER</h2>
-            <button
-                id="options__button"
-                type="button"
-                onClick={(): Promise<Tabs.Tab> => {
-                    return openWebPage('options.html');
-                }}
-            >
-                Options Page
-            </button>
-            <div className="links__holder">
-                <ul>
-                    <li>
-                        <button
-                            type="button"
-                            onClick={(): Promise<Tabs.Tab> => {
-                                return openWebPage(
-                                    'https://github.com/abhijithvijayan/web-extension-starter',
-                                );
-                            }}
-                        >
-                            GitHub
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            type="button"
-                            onClick={(): Promise<Tabs.Tab> => {
-                                return openWebPage(
-                                    'https://www.buymeacoffee.com/abhijithvijayan',
-                                );
-                            }}
-                        >
-                            Buy Me A Coffee
-                        </button>
-                    </li>
-                </ul>
+        <section id="popup" className="w-96 h-full bg-white">
+            <div className="py-6 px-4 flex flex-col justify-start gap-4">
+                <h2 className="text-xl w-full text-center">Alt CDN</h2>
+                <div className="flex flex-row justify-evenly">
+                    <button
+                        id="options__button"
+                        type="button"
+                        onClick={(): Promise<Tabs.Tab> => {
+                            return openWebPage('options.html');
+                        }}
+                        className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Options
+                    </button>
+                    <button
+                        type="button"
+                        onClick={(): Promise<Tabs.Tab> => {
+                            return openWebPage(
+                                'https://github.com/sandstone991/alt-cdn',
+                            );
+                        }}
+                        className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
+                    >
+                        GitHub
+                    </button>
+                </div>
             </div>
         </section>
     );
